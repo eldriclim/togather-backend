befb9a6a09b532a6de6a49f96ba1217e655ae4f6
Eldric <eldriclim93@gmail.com>
2019-01-19 13:23:25 +0800

    Set up express

---
+++ server.js
@@ -0,0 +1,17 @@
+require('./config/config');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+var app = express();
+const port = process.env.PORT;
+
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello world');
+});
+
+app.listen(port, () => {
+  console.log(`Started on port ${port}`);
+});
